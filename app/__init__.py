744e18317ebf8cd11331922d14711ab7120bd58c
I306454 <jiabin.zheng01@sap.com>
2018-06-17 23:47:37 +0800

    finish ajax translation

--- app/__init__.py
+++ app/__init__.py
@@ -53,7 +53,7 @@ if not app.debug:
 
 @babel.localeselector
 def get_local():
-    #return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'es'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    #return 'es'
 
 from app import routes, models, errors
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect, url_for, request, g
+from flask import render_template, flash, redirect, url_for, request, g, jsonify
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, PostForm,\
 ResetPasswordRequestForm, ResetPasswordForm
@@ -8,6 +8,8 @@ from werkzeug.urls import url_parse
 from datetime import datetime
 from app.email import send_password_reset_email
 from flask_babel import get_locale
+from guess_language import guess_language
+from app.translate import translate
 
 
 @app.before_request
@@ -24,7 +26,12 @@ def defore_request():
 def index():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(body=form.post.data, author=current_user)
+        language = guess_language(form.post.data)
+        print('guess language: %s' %language)
+        if language == 'UNKNOWN' or len(language) > 5:
+            language = ''
+
+        post = Post(body=form.post.data, author=current_user, language=language)
         db.session.add(post)
         db.session.commit()
         flash('Your post is now live!')
@@ -175,3 +182,10 @@ def reset_password(token):
         flash('Your password has been reset.')
         return redirect(url_for('login'))
     return render_template('reset_password.html', form=form)
+
+@app.route('/translate', methods=['POST'])
+@login_required
+def translate_text():
+    return jsonify({'text': translate(request.form['text'],
+                                      request.form['source_language'],
+                                      request.form['dest_language'])})
